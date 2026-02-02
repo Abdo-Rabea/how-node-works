6ca8ac91da4d3298835c35099031b7bafd535350
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2026-02-02 23:33:03 +0200

    feat: http test events

--- events.js
+++ events.js
@@ -19,3 +19,18 @@ salesEmitter.on('new-sale', (arg) => {
 });
 
 salesEmitter.emit('new-sale', 8);
+
+// HTTP server test events
+const http = require('http');
+const server = http.createServer();
+//* the server emits event named request whenever there a request hits the server
+server.on('request', (req, res) => {
+  res.end('hello world');
+});
+server.on('request', (req, res) => {
+  console.log(req.url);
+  console.log('hello there');
+});
+
+// you should listen
+server.listen(8000, '127.0.0.1');
